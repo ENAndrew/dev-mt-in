1669d84548856c0086c9c5f90db4128685dc8b4f
Erica Andrew <erica.nicole.andrew@gmail.com>
2016-02-18 20:21:52 -0700

    Working on Day 3 - profileService.checkForProfile not invoking.

--- js/homeCtrl.js
+++ js/homeCtrl.js
@@ -5,13 +5,14 @@
     angular.module('devMtIn').controller('homeCtrl', function($scope, profileService){
         
         $scope.editing = false;
-
-        $scope.myProfile = {
-            friends: [{name: 'Bryan'}, {name: 'Ryan'}, {name: 'Sarah'}, {name: 'Zac'}, {name: 'Erin'}],
-            myName: '',
-            myLikes: '',
-            myFavColor: ''
-        };
+        
+        //this is now held in localStorage via the service
+//        $scope.myProfile = {
+//            friends: [{name: 'Bryan'}, {name: 'Ryan'}, {name: 'Sarah'}, {name: 'Zac'}, {name: 'Erin'}],
+//            name: '',
+//            myLikes: '',
+//            myFavColor: ''
+//        };
         
         $scope.sortOptions = [{
             display: 'Ascending',
@@ -25,12 +26,28 @@
     
         profileService.serviceTest();
         
+        $scope.checkForProfile = function() {
+          var profileId = JSON.parse(localStorage.getItem('profileId'));
+          if(profileId) {
+              profileService.checkForProfile(profileId.profileId)
+                      .then(function(profile){
+                          $scope.myProfile = profile.data;
+              })
+                      .catch(function(err){
+                          console.error(err);
+              });
+          }
+          console.log('checkForProfile ran');
+        }();  //immediately invoked
+        
         $scope.saveProfile = function(profile) {
-            
-            profile.Service.saveProfile(profile);
-            console.log($scope.myProfile);
-            $scope.editing = false;
-            
+            profileService.saveProfile(profile);
+            $scope.editing = false;        
+        };
+        
+        $scope.deleteProfile = function() {
+            profileService.deleteProfile();
+            $scope.myProfile = profileService.checkForProfile();
         };
         
        
--- js/profileService.js
+++ js/profileService.js
@@ -2,15 +2,43 @@
 
 (function(){
     
-    angular.module('devMtIn').service('profileService', function(){
+    angular.module('devMtIn').service('profileService', function($http){
+        
+        this.baseUrl = 'http://connections.devmounta.in/';
+        
+        this.checkForProfile = function(profileId){
+            return $http({
+                method: 'GET',
+                url: this.baseUrl + 'api/profiles/' + profileId
+            });
+             console.log('service.checkForProfile checked');
+//          if(localStorage.getItem('profile')) {
+//            return JSON.parse(localStorage.getItem('profile'));  
+//          };
+        };
+      
         
         this.serviceTest = function() {
             console.log('profileService is connected');
         };
         
         this.saveProfile = function(profile) {
-            localStorage.setItem('profile', JSON.stringify(profile));
-        }
+            return $http({
+               method: 'POST', //request method
+               url: this.baseUrl + 'api/profiles', //url we are making the request to
+               data: profile  //the data we are requesting to be posted
+            }).then(function(profileResponse){
+                localStorage.setItem('profile', JSON.stringify({profileId:profileResponse.data._id}));
+            }).catch(function(err){
+                console.error(err);
+            });
+        };
+        
+        this.deleteProfile = function() {
+            localStorage.removeItem('profile');
+        };
+        
+
         
     });
     
